632d29fcda7b1c458afe8235a76616ae11810b1c
effner <b.effner@tu-braunschweig.de>
2016-06-02 16:29:27 +0200

    Knoten werden ohne doppelung hinzugefügt in einer liste

--- Main.java
+++ Main.java
@@ -5,16 +5,12 @@ import java.nio.file.Paths;
 
 public class Main {
 
-    private static DoubleLinkedList<Edge> graph1 = new DoubleLinkedList<>();
+	private static DoubleLinkedList<Edge> edges = new DoubleLinkedList<>();
+	private static DoubleLinkedList<Vertex> vertices = new DoubleLinkedList<>();
 
 	public static void main(String[] args) {
         readGraph("graph.txt");
-        System.out.println("unsortierte graph");
-        System.out.println(graph1.toString());
-        reversDelete();
-        System.out.println("sortierter graph");
-        System.out.println(graph1.toString());
-        
+        System.out.println(vertices);
 	}
 
     private static void readGraph(String filename){
@@ -25,7 +21,10 @@ public class Main {
             String[] data;
             while ((line = bufferedReader.readLine()) != null) {
                 data = line.split(";");
-                graph1.insert(new Edge(data[0], data[1], data[2]));
+                edges.insert(new Edge(data[0], data[1], data[2]));
+          //      System.out.println(data[0] + data[1]);
+                insertVertex(data[0]); 
+                insertVertex(data[1]);
 
             }
             bufferedReader.close();
@@ -36,16 +35,35 @@ public class Main {
         }
     }
 
-    private static void reversDelete(){
+    private static void reversDelete(DoubleLinkedList graph){
         int i = 1;
 
-        while (i < graph1.length()) {
+        while (i < graph.length()) {
             System.out.println(i);
-            graph1.delete(graph1.get(i));
+            graph.delete(graph.get(i));
             //if (edge.v1 is not connected to edge.v2){
               // E[i] edge
             //}
             i++;
         }
+    }
+    
+    public static boolean insertVertex(String data){
+    	char a = data.charAt(0);
+    	//System.out.println("suche " + a);
+    	int i=0;
+        while (i <= vertices.length()) {	
+        	//System.out.println("   prüfe position " + i);
+            if (vertices.get(i) != null && 
+            		vertices.get(i).getVertex() == a) {
+            	//System.out.println(i + " in liste ist " + a);
+                return true;
+			}
+            i++;
+        }
+        //System.out.println("Füge ein " + a);
+        vertices.insert(new Vertex(a));
+        return false;
+    	
     }
 }
